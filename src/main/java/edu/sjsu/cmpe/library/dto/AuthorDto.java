6bbf2e37db1bd8937860e8453e7d6cb0125d097d
preetiborade <preeti.borade@gmail.com>
2014-03-08 23:19:04 +0530

    authors added

---
+++ src/main/java/edu/sjsu/cmpe/library/dto/AuthorDto.java
@@ -0,0 +1,32 @@
+package edu.sjsu.cmpe.library.dto;
+
+import edu.sjsu.cmpe.library.domain.Author;
+
+public class AuthorDto extends LinksDto {
+	private Author author;
+
+	public AuthorDto(Author author) {
+		super();
+		this.author = author;
+	    }
+
+	public AuthorDto(){
+
+    }
+
+
+	/**
+	 * @return the author
+	 */
+	public Author getAuthor() {
+		return author;
+	}
+
+	/**
+	 * @param author the author to set
+	 */
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+}
